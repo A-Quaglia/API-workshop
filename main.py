ffeb95996f940036374f3618c4e2e3d4f159cc24
A-Quaglia <augustoquaglia@outlook.com>
2024-03-27 22:59:00 -0300

    feat: endpoint produtos

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
+from typing import Dict, List
 from fastapi import FastAPI
 
 app = FastAPI()
 
+produtos: List[Dict[str, any]] = [
+    dict(id=1,
+        nome="Smartphone",
+        descricao="Um telefone que é inteligente",
+        preco=1500.0,
+        disponivel=True),
+
+        dict(id=2,
+        nome="Notebook",
+        descricao="Um computador que é móvel",
+        preco=4500.0,
+        disponivel=True),
+
+        dict(id=3,
+        nome="Tablet",
+        descricao="Um Tablet",
+        preco=3500.0,
+        disponivel=True)
+]
+
+
 @app.get('/') # Request
 def ola_mundo(): # Response
     return {"Olá": "Mundo"}
+
+@app.get("/produtos")
+def listar_produtos():
+    return produtos
